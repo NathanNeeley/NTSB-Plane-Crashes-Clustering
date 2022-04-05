3d4cca2ff9c5cb0b76cd19800bcb3698b8a17ee4
NathanNeeley <37356069+NathanNeeley@users.noreply.github.com>
2022-04-05 00:13:11 -0400

    Add files via upload

--- files.py
+++ files.py
@@ -1,29 +1,41 @@
 import os
 import json
 import math
+import datetime
+import time
 import spacy
 import nltk
 import pyLDAvis
 from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 from collections import Counter
 from nltk.corpus import stopwords
 
 # Data
 data_folder = 'Downloaded_PlaneReports'
-mdl_file = 'trained-mdl.pkl'
+mdl_file = 'trained-mdl'
 
-def preprocess():
+def preprocess(start_date=None, end_date=None):
 
     docs = []           # List of documents
     vocab = Counter()   # Counter keys are terms, values are term frequencies
 
+    lemmatizer = WordNetLemmatizer()
     nltk.download('punkt')
     nltk.download('stopwords')
+    nltk.download('wordnet')
+    nltk.download('omw-1.4')
+    nltk.download('averaged_perceptron_tagger')
     en = spacy.load('en_core_web_sm')
 
+    # Convert dates
+    if start_date != None: start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    if end_date != None: end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    if end_date == None: end_date = datetime.datetime.now()
+
     # Words to remove from vocab
-    remove_words = ["'s", 'pilot', 'airplane', 'plane', 'due', 'could', 'resulted', 'failure', 'landing', 'loss',
-                    'maintain', 'control', 'directional', 'flight', 'reasons', 'determined', 'runway']
+    remove_words = ['pilot', 'factor', 'contributing', 'aircraft', 'airplane', 'plane', 'due', 'could', 'would', 'resulted', 'failure', 'landing',
+                    'loss', 'maintain', 'control', 'directional', 'flight', 'reasons', 'determined', 'runway']
 
     # Extract all probable cause lines from json files
     files = os.listdir(data_folder)
@@ -31,15 +43,24 @@ def preprocess():
         f = open(os.path.join(data_folder, file), encoding='utf8')
         file_contents = json.load(f)
         for crash in file_contents:
+            inDateRange = False
             for tag in crash.keys():
-                if tag == 'cm_probableCause':
+                if tag == 'cm_probableCause' and crash[tag] != None:
                     tokens = word_tokenize(crash[tag])
-                    tokens = [token.lower() for token in tokens if token.lower() not in stopwords.words('english')
-                              and len(token) > 1 and token not in remove_words]
+                    if (len(tokens) > 0):
+                        tokens = [lemmatizer.lemmatize(token.lower()) for token in tokens]
+                        tokens = [token.lower() for token in tokens if token.lower() not in stopwords.words('english') and len(token) > 2]
+                        tokens = [token for token in tokens if token not in remove_words]
+                elif tag == 'cm_eventDate':
+                    event_date = crash[tag][0:10]
+                    event_date = datetime.datetime.strptime(event_date, '%Y-%m-%d')
+                    if (start_date == None or (start_date <= event_date <= end_date)): # Check date range
+                        inDateRange = True
 
-                    # Add tokens to docs list and update vocab numbers and data
-                    docs.append(tokens)
-                    vocab.update(tokens)
+            if inDateRange == True and len(tokens) > 0:
+                # Add tokens to docs list and update vocab numbers and data
+                docs.append(tokens)
+                vocab.update(tokens)
 
     return docs, vocab
 
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import warnings
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
 import os
 import pickle
 import numpy as np
@@ -9,6 +12,16 @@ from GSDMM import GSDMM
 
 if __name__ == '__main__':
     data, mdl, docs, vocab = None, None, None, None
+    start_date = None
+    end_date = None
+
+    # Change name of file based on start and end dates
+    if (start_date != None):
+        files.mdl_file = files.mdl_file + '_' + start_date + '_' + end_date + '.pkl'
+        vis_file_name = 'clusters-vis_' + start_date + '_' + end_date + '.html'
+    else:
+        files.mdl_file = files.mdl_file + '.pkl'
+        vis_file_name = 'clusters-vis.html'
 
     # Check if there's a saved model
     if os.path.exists(files.mdl_file):
@@ -19,12 +32,12 @@ if __name__ == '__main__':
             docs = data['docs']
             vocab = data['vocab']
     else:
-        # TODO -- run web crawler
-
         # Extract data from json files, tokenize it, and remove non-descriptive words
-        docs, vocab = preprocess()
+        print("Preprocessing")
+        docs, vocab = preprocess(start_date=start_date, end_date=end_date)
 
         # Fit data and group into clusters
+        print("Clustering")
         mdl = GSDMM()
         model = mdl.fit(docs, len(vocab))
         mdl = GSDMM.from_data(K=10, alpha=0.1, beta=0.1, D=len(docs), vocab_size=len(vocab), cluster_doc_count=mdl.cluster_doc_count, cluster_word_count=mdl.cluster_word_count, cluster_word_distribution=mdl.cluster_word_distribution)
@@ -50,4 +63,4 @@ if __name__ == '__main__':
 
     # Visualize vocab and clusters
     vis_data = prepare_vis_data(mdl, docs, vocab, K=10)
-    pyLDAvis.save_html(vis_data, 'clusters-vis.html')
+    pyLDAvis.save_html(vis_data, vis_file_name)
